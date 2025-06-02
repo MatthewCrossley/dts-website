bd9527c00709342496ea24c237c9f76f72eabe3b
Matthew Crossley <matthew.crossley@bp.com>
2025-06-02 16:44:47 +0100

    Add indication of completion to homepage

--- frontend/src/components/task.js
+++ frontend/src/components/task.js
@@ -2,7 +2,7 @@ export default function Task({ task }) {
   return (
     <a href={`/task/${task.id}`}>
       <div class="task">
-        <h3>{task.title}</h3>
+        <h3>[{task.completed ? "✓" : " "}]{task.title}</h3>
         <p>{task.description}</p>
         <p>Due: {task.due ? new Date(task.due).toLocaleString() : "No due date"}</p>
       </div>
